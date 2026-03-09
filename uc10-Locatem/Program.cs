72302488c89ffaf1e5e32a91fbc1741b37859523
claudio.gbgomes <claudio.gbgomes@TIT0676447W11-1>
2026-03-09 17:06:27 -0300

    Removido WeatherForecast e adicionado Scalar.AspNetCore
    WeatherForecastController e classe WeatherForecast removidos.
    Adicionada dependência Scalar.AspNetCore e configuração MapScalarApiReference no Program.cs.
    Atualizado uc10-Locatem.csproj com novas dependências e pasta Controllers.

--- uc10-Locatem/Program.cs
+++ uc10-Locatem/Program.cs
@@ -1,4 +1,6 @@
 
+using Scalar.AspNetCore;
+
 namespace uc10_Locatem
 {
     public class Program
@@ -19,6 +21,7 @@ namespace uc10_Locatem
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
